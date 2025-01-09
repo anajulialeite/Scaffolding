1651ec0a776e7a3b696f35d7e95249ed9bae3821
Ana Júlia Leite <leite.anajulia25@gmail.com>
2025-01-09 00:29:16 -0300

    Acrescentando os dados do Scaffolding

---
+++ DATABASE_FIRST/DATABASE_FIRST/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DATABASE_FIRST.Models;
+
+public partial class Order
+{
+    public int Id { get; set; }
+
+    public DateTime Timeorder { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public int? IdUsers { get; set; }
+
+    public virtual User? IdUsersNavigation { get; set; }
+
+    public virtual ICollection<Item> IdItems { get; set; } = new List<Item>();
+}
